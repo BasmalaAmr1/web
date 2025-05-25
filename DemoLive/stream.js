9445710feb7be40c4bae07758fc6dc979bc0cd19
BasmalaAmr1 <148448444+BasmalaAmr1@users.noreply.github.com>
2025-05-25 11:48:44 +0300

    Add files via upload

---
+++ DemoLive/stream.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected: ", socket.id);
+
+  socket.on("offer", (data) => {
+    socket.broadcast.emit("offer", data);
+  });
+
+  socket.on("answer", (data) => {
+    socket.broadcast.emit("answer", data);
+  });
+
+  socket.on("candidate", (data) => {
+    socket.broadcast.emit("candidate", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected: ", socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Live streaming server running on http://localhost:3000");
+});
